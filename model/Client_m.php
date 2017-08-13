aefe4d24c08d8ed319116f1d822f462075d9b6ef
sitrakiniaina <rasendrahasinarivo.herinihaja@gmail.com>
2017-08-13 12:35:27 +0300

    structuration du projet

---
+++ model/Client_m.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Client_m
+ *
+ * @author herinihaja
+ */
+class Client_m {
+    //put your code here
+}
